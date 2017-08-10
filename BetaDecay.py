905c642d357e6f480d07a0c998548d05cf84bf3c
Marcus O'Flaherty <moflaherty@sheffield.ac.uk>
2017-08-10 17:20:31 -0500

    first commit

---
+++ BetaDecay.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pylab as P
+import math 
+
+Ns = 1.47e-13
+
+def Energy(mass,Ke):
+    return mass + Ke
+
+def Momentum(mass,Ke):
+    return math.sqrt(Energy(mass,Ke)**2 - mass**2)
+
+def beta(Ke,Q,Mixing,Masses):
+    Ns*Fermi(2,Ke)*Ee*pe*
