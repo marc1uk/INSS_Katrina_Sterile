01a79391a384e637d0a9dd98bc64a350a313ab79
xzhan135 <xzhan135@hawk.iit.edu>
2017-08-10 17:48:13 -0500

    Added a vanilla fermi function.

--- BetaDecay.py
+++ BetaDecay.py
@@ -3,6 +3,8 @@ import pylab as P
 import math 
 
 Ns = 1.47e-13
+mass = 0.511
+kb = 8.76e-8
 
 def Energy(mass,Ke):
     return mass + Ke
@@ -14,3 +16,8 @@ def beta(Ke,Q,Mixing,Masses):
     Ns*Fermi(2,Ke)*Ee*pe*
 
 #testing
+<<<<<<< HEAD
+def Fermi(Z,Ke)
+		1/(1+np.exp(Ke+mass-Ef(Z))/Ke*kb))
+=======
+>>>>>>> 785383b416f64c7323e51807bf105f35c6eae15c
