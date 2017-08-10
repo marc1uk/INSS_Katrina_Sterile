5dc3be579a34718fd25b45e1929e50f838389623
xzhan135 <xzhan135@hawk.iit.edu>
2017-08-10 17:49:19 -0500

    Cleaned some warnings.

--- BetaDecay.py
+++ BetaDecay.py
@@ -16,8 +16,5 @@ def beta(Ke,Q,Mixing,Masses):
     Ns*Fermi(2,Ke)*Ee*pe*
 
 #testing
-<<<<<<< HEAD
 def Fermi(Z,Ke)
 		1/(1+np.exp(Ke+mass-Ef(Z))/Ke*kb))
-=======
->>>>>>> 785383b416f64c7323e51807bf105f35c6eae15c
